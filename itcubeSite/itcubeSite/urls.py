c36bee20fe15a050f201a4eab498443745bc1c44
Viltskaa <andrewstyou@gmail.com>
2023-04-19 19:03:31 +0400

    edit models + views

--- itcubeSite/itcubeSite/urls.py
+++ itcubeSite/itcubeSite/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('new/<name>', views.product_new),
+    path('products', views.product_list),
 ]
--- itcubeSite/itcubeSite/views.py
+++ itcubeSite/itcubeSite/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def homePage(request):
@@ -8,3 +9,23 @@ def homePage(request):
 
 def index(request):
     return render(request, 'index.html')
+
+
+def product_new(request, name="pomidor"):
+    try:
+        product = Product(name=name)
+        product.save()
+        return HttpResponse(f"<h3>id: {product.id} name: {product.name}</h3>")
+    except Exception as e:
+        return HttpResponse(f"<h1><b>{e}</b></h1>")
+
+
+def product_list(request):
+    out = Product.objects.all()
+    outHtml = '<div>'
+
+    for i in out:
+        outHtml += "<h3>" + i.name + "</h3>" + "<br>"
+
+    outHtml += '</div>'
+    return HttpResponse(outHtml)
