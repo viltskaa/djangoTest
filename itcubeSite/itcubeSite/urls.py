e1412cc304353fe223cf2ca931aec601a21a3053
Viltskaa <andrewstyou@gmail.com>
2023-04-22 15:41:08 +0400

    add templates

--- itcubeSite/itcubeSite/urls.py
+++ itcubeSite/itcubeSite/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('new/<name>', views.product_new),
     path('products', views.product_list),
+    path('name/<name>', views.my_name),
+    path('product/<id>')
 ]
--- itcubeSite/itcubeSite/views.py
+++ itcubeSite/itcubeSite/views.py
@@ -11,6 +11,25 @@ def index(request):
     return render(request, 'index.html')
 
 
+def my_name(request, name: str):
+    vowels = "aeiyou"
+
+    name_to_data = []
+    for letter in name:
+        is_vowel = False
+        if letter.lower() in vowels:
+            is_vowel = True
+
+        name_to_data.append(
+            [letter, is_vowel]
+        )
+
+    data = {
+        "word": name_to_data
+    }
+    return render(request, 'test.html', data)
+
+
 def product_new(request, name="pomidor"):
     try:
         product = Product(name=name)
